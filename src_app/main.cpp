32aecfd7c9ae8fdf0da0bbfb99ca3bd1fb5a445c
Carlo R. Carrano <crc@dazzlingsolutions.com>
2015-05-21 23:37:50 -0500

    Changed the distribution license information in all the files.

--- src_app/main.cpp
+++ src_app/main.cpp
@@ -3,14 +3,14 @@
  *   crc@dazzlingsolutions.com                                            *
  *                                                                        *
  *   This program is free software; you can redistribute it and/or modify *
- *   it under the terms of the GNU General Public License as published by *
- *   the Free Software Foundation; version 2 of the License.              *
+ *   it under the terms of the GNU Lesser General Public License as       *
+ *   published by the Free Software Foundation; version 3 of the License. *
  *   This program is distributed in the hope that it will be useful, but  *
  *   WITHOUT ANY WARRANTY; without even the implied warranty of           *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     *
- *   General Public License for more details.                             *
- *   You should have received a copy of the GNU General Public License    *
- *   along with this program; if not, write to the Free Software          *
+ *   Lesser General Public License for more details.                      *
+ *   You should have received a copy of the Lesser GNU General Public     *
+ *   License along with this program; if not, write to the Free Software  *
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  *
  *   USA.  																  *
  **************************************************************************/
@@ -21,6 +21,7 @@
 /**************************************************************************
  * Change Log                                                             *
  *------------------------------------------------------------------------*
+ * 05-21-2015	modified distribution license                             *
  * 03-07-2015	file created                                              *
  **************************************************************************/
 
